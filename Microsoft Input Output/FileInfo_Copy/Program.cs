6ec66e184f478dce04b62811d40b25c9e8901963
zorayrbaghdasaryan <zorayrbaghdasaryan@gmail.com>
2019-01-28 16:03:03 +0400

    FileInfo_Copy

---
+++ Microsoft Input Output/FileInfo_Copy/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+
+// Копирование файлов.
+
+namespace FileInfo_Copy
+{
+    class Program
+    {
+        static void Main()
+        {
+            // Создаем объект для работы с файлом.
+            var file = new FileInfo(@"C:\Windows\notepad.exe");
+
+            //     var dir = new DirectoryInfo(".");
+
+            // Копируем содержимое файла.
+            try
+            {
+                file.CopyTo(@"D:\notepad.exe");
+                Console.WriteLine("Файл успешно скопирован!");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            // Delay.
+            Console.ReadKey();
+        }
+    }
+}
