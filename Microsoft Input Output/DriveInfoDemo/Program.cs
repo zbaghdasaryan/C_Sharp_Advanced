93845a20d9b553d1b04ba2554372d79eaec98cde
zorayrbaghdasaryan <zorayrbaghdasaryan@gmail.com>
2019-01-28 16:05:06 +0400

    DriveInfoDemo

---
+++ Microsoft Input Output/DriveInfoDemo/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+
+// Получение информации о дисках.
+
+namespace DriveInfoDemo
+{
+    class Program
+    {
+        static void Main()
+        {
+            // Создание коллекции дисков.
+            DriveInfo[] drives = DriveInfo.GetDrives();
+
+            // Вывод информации о дисках компьютера.
+            foreach (DriveInfo drive in drives)
+            {
+                Console.WriteLine("Drive: {0} Type: {1}", drive.Name, drive.DriveType);
+            }
+
+            // Delay.
+            Console.ReadKey();
+        }
+    }
+}
