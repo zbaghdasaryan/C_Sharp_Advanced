db061fac9600a99457d9b5445c2158b7735a7d37
zorayrbaghdasaryan <zorayrbaghdasaryan@gmail.com>
2019-02-07 21:17:03 +0400

    Asinc Await Cotinuation

---
+++ Async Await/Asinc Await Continuation/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Async_Await_Continuation
+{
+    class MyClass
+    {
+        void Operation()
+        {
+            Thread.Sleep(2000);
+            Console.WriteLine("Main Task  {0}", Thread.CurrentThread.ManagedThreadId);
+        }
+
+        public async Task OperationAsinc()
+        {
+            await Task.Factory.StartNew(Operation);
+                    }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass myClass = new MyClass();
+            Task task = myClass.OperationAsinc();
+            task.ContinueWith(t => Console.WriteLine("\nContinuation task"));
+           
+            Console.ReadKey();
+
+        }
+    }
+}
