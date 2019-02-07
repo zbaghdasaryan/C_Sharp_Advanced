ae9c6a8d1eabbd3eb1e4e0128587b55ef9f9c92c
zorayrbaghdasaryan <zorayrbaghdasaryan@gmail.com>
2019-02-07 20:33:52 +0400

    Asinc Await Task 3

---
+++ Async Await/Asinc Await Task 2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Async_Await
+{
+    class MyClass
+    {
+        public void Operation()
+        {
+            Console.WriteLine("Operation Thread ID {0}", Thread.CurrentThread.ManagedThreadId);
+            Console.WriteLine("Begin");
+            Thread.Sleep(2000);
+            Console.WriteLine("End");
+        }
+
+        public async void OperationAsinc()
+        {
+            Console.WriteLine("OperationAsinc (Part 1) Thread ID {0}", Thread.CurrentThread.ManagedThreadId);
+            Task task = new Task(Operation);
+            task.Start();
+            await task;
+            Console.WriteLine("\nOperationAsinc (Part 2) Thread ID {0}", Thread.CurrentThread.ManagedThreadId);
+        }
+
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Mail ThreadID {0}", Thread.CurrentThread.ManagedThreadId);
+            MyClass myClass = new MyClass();
+            myClass.OperationAsinc();
+        }
+    }
+}
---
+++ Async Await/Asinc Await Task 3/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Async_Await_Task_3
+{
+    class MyClass
+    {
+        int Operation()
+        {
+            Console.WriteLine("Operation Thread ID {0}", Thread.CurrentThread.ManagedThreadId);
+
+            Thread.Sleep(2000);
+            return 2 * 2;
+        }
+
+        public async void OperationAsinc()
+        {
+            Console.WriteLine("OperationAsinc (Part 1) Thread ID {0}", Thread.CurrentThread.ManagedThreadId);
+            Task<int> task = Task<int>.Factory.StartNew(Operation);
+            Console.WriteLine("\nresult {0}", await task);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Mail ThreadID {0}", Thread.CurrentThread.ManagedThreadId);
+            MyClass myClass = new MyClass();
+            myClass.OperationAsinc();
+        }
+    }
+}
