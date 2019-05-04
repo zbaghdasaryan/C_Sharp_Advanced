040401edc0ec67015c54265ce5701a3cbf0e950a
zorayrbaghdasaryan <zorayrbaghdasaryan@gmail.com>
2019-05-04 19:04:13 +0400

    Queue<T>

---
+++ Collections/Queue_T/Program.cs
@@ -0,0 +1,61 @@
+﻿/*
+ * Класс Queue<T> представляет обычную очередь, работающую по алгоритму FIFO ("первый вошел - первый вышел").
+
+У класса Queue<T> можно отметить следующие методы:
+
+Dequeue: извлекает и возвращает первый элемент очереди
+
+Enqueue: добавляет элемент в конец очереди
+
+Peek: просто возвращает первый элемент из начала очереди без его удаления
+*/
+
+using System;
+using System.Collections.Generic;
+
+namespace Collections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<int> numbers = new Queue<int>();
+
+            numbers.Enqueue(3); // очередь 3
+            numbers.Enqueue(5); // очередь 3, 5
+            numbers.Enqueue(8); // очередь 3, 5, 8
+
+            // получаем первый элемент очереди
+            int queueElement = numbers.Dequeue(); //теперь очередь 5, 8
+            Console.WriteLine(queueElement);
+
+            Queue<Person> persons = new Queue<Person>();
+            persons.Enqueue(new Person() { Name = "Tom" });
+            persons.Enqueue(new Person() { Name = "Bill" });
+            persons.Enqueue(new Person() { Name = "John" });
+
+            // получаем первый элемент без его извлечения
+            Person pp = persons.Peek();
+            Console.WriteLine(pp.Name);
+
+            Console.WriteLine("Сейчас в очереди {0} человек", persons.Count);
+
+            // теперь в очереди Tom, Bill, John
+            foreach (Person p in persons)
+            {
+                Console.WriteLine(p.Name);
+            }
+
+            // Извлекаем первый элемент в очереди - Tom
+            Person person = persons.Dequeue(); // теперь в очереди Bill, John
+            Console.WriteLine(person.Name);
+
+            Console.ReadLine();
+        }
+    }
+
+    class Person
+    {
+        public string Name { get; set; }
+    }
+}
