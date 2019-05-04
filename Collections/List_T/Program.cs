4efcec925e28ee945a7a33f379167e845993a4bc
zorayrbaghdasaryan <zorayrbaghdasaryan@gmail.com>
2019-05-04 18:58:10 +0400

    List<T>

---
+++ Collections/List_T/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+ 
+namespace Collections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int>() { 1, 2, 3, 45 };
+            numbers.Add(6); // добавление элемента
+
+            numbers.AddRange(new int[] { 7, 8, 9 });
+
+            numbers.Insert(0, 666); // вставляем на первое место в списке число 666
+
+            numbers.RemoveAt(1); //  удаляем второй элемент
+
+            foreach (int i in numbers)
+            {
+                Console.WriteLine(i);
+            }
+
+            List<Person> people = new List<Person>(3);
+            people.Add(new Person() { Name = "Том" });
+            people.Add(new Person() { Name = "Билл" });
+
+            foreach (Person p in people)
+            {
+                Console.WriteLine(p.Name);
+            }
+
+            Console.ReadLine();
+        }
+    }
+
+    class Person
+    {
+        public string Name { get; set; }
+    }
+}
